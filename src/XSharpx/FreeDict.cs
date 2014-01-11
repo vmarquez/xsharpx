d37cc7ebdea36011bbe8ecfa66060569a7c14281
Vincent <vincent.marquez@gmail.com>
2014-01-10 18:07:06 -0800

    no longer mutating in the Add/Get methods

--- src/XSharpx/FreeDict.cs
+++ src/XSharpx/FreeDict.cs
@@ -8,17 +8,33 @@ namespace XSharpx
 
     public class FreeDict<A, B, C>
     {
-
-        public readonly Either<Func<Dictionary<A, B>, C>, Func<Dictionary<A, B>, FreeDict<A, B, C>>> maybeMore;
-
-        public FreeDict(Func<Dictionary<A, B>, C> f)
+        public readonly Either<C, Either<Func<Tuple<A, B, FreeDict<A, B, C>>>, Func<Tuple<A, Func<B,FreeDict<A, B, C>>>>>> maybeGetPushMore;
+       
+        public FreeDict(C c) //Done
         {
-            maybeMore = Either<Func<Dictionary<A, B>, C>, Func<Dictionary<A, B>, FreeDict<A, B, C>>>.Left(f);
+            maybeGetPushMore = Either<C, Either<Func<Tuple<A, B, FreeDict<A, B, C>>>, Func<Tuple<A, Func<B, FreeDict<A, B, C>>>>>>.Left(c);
         }
 
-        public FreeDict(Func<Dictionary<A, B>, FreeDict<A, B, C>> f)
+        public FreeDict(Func<Tuple<A, B, FreeDict<A, B, C>>> f) //Add+More
         {
-            maybeMore = Either<Func<Dictionary<A, B>, C>, Func<Dictionary<A, B>, FreeDict<A, B, C>>>.Right(f);
+            maybeGetPushMore = Either<C, Either<Func<Tuple<A, B, FreeDict<A, B, C>>>, Func<Tuple<A, Func<B,FreeDict<A, B, C>>>>>>
+                 .Right(Either<Func<Tuple<A, B, FreeDict<A, B, C>>>, Func<Tuple<A, Func<B, FreeDict<A, B, C>>>>>.Left(f));
+        }
+
+        public FreeDict(Func<Tuple<A, Func<B,FreeDict<A, B, C>>>> f)//Get+More
+        {
+            maybeGetPushMore = Either<C, Either<Func<Tuple<A, B, FreeDict<A, B, C>>>, Func<Tuple<A, Func<B, FreeDict<A, B, C>>>>>>
+                .Right(Either<Func<Tuple<A, B, FreeDict<A, B, C>>>, Func<Tuple<A, Func<B, FreeDict<A, B, C>>>>>.Right(f));
+        }
+
+        public static FreeDict<A, B, Unit> Add(A a, B b)
+        {
+            return new FreeDict<A, B, Unit>(()=>new Tuple<A,B,FreeDict<A,B,Unit>>(a, b, new FreeDict<A,B,Unit>(new Unit())));
+        }
+
+        public static FreeDict<A, B, B> Get(A a)
+        {
+            return new FreeDict<A,B,B>( ()=> new Tuple<A,Func<B,FreeDict<A,B,B>>>(a, (b)=>new FreeDict<A,B,B>(b)));
         }
 
         //UNSAFE
@@ -30,45 +46,29 @@ namespace XSharpx
             }
         }
 
-        private C RunRec(Dictionary<A, B> t)
+        private C RunRec(Dictionary<A, B> dict)
         {
-
-            return maybeMore.Fold(f => f(t), mf =>
+            return maybeGetPushMore.Swap.Reduce(mf => mf.Fold(addf =>
             {
-                var fd = mf(t);
-                return fd.RunRec(t);
-            });
-        }
-
-        public static FreeDict<A, B, Unit> Add(A a, B b)
-        {
-            return new FreeDict<A, B, Unit>(dic =>
+                var t = addf();
+                dict.Add(t.Item1, t.Item2);
+                return t.Item3.RunRec(dict);
+            },
+            getf =>
             {
-                dic.Add(a, b);
-                return new Unit();
-            });
-        }
-
-        public static FreeDict<A, B, B> Get(A a)
-        {
-            return new FreeDict<A, B, B>(dic => dic[a]);
+                var t = getf();
+                var b = dict[t.Item1];
+                return t.Item2(b).RunRec(dict);
+            }));
         }
     }
 
     public static class FreeDictExt
     {
-
-        public static FreeDict<A, B, D> Select<A, B, C, D>(this FreeDict<A, B, C> fd, Func<C, D> select)
+        public static FreeDict<A, B, D> Select<A, B, C, D>(this FreeDict<A, B, C> fd, Func<C, D> f)
         {
-            return fd.maybeMore.Fold(f =>
-            {
-                Func<Dictionary<A, B>, D> testf = (dic) => select(f(dic)); //type inference fails on constructor?
-                return new FreeDict<A, B, D>(testf);
-            },
-            mf =>
-               new FreeDict<A, B, D>((dic) => mf(dic).Select(c => select(c))));
+            return fd.SelectMany(c => new FreeDict<A, B, D>(f(c)));
         }
-
 
         public static FreeDict<A, B, E> SelectMany<A, B, C, D, E>(this FreeDict<A, B, C> fd, Func<C, FreeDict<A, B, D>> bind, Func<C, D, E> select)
         {
@@ -77,16 +77,18 @@ namespace XSharpx
 
         public static FreeDict<A, B, D> SelectMany<A, B, C, D>(this FreeDict<A, B, C> fd, Func<C, FreeDict<A, B, D>> bind)
         {
-            return fd.maybeMore.Fold(f =>
-            new FreeDict<A, B, D>((dic) =>
-               bind(f(dic))
-                ),
-            mf =>
-                new FreeDict<A, B, D>((dic) =>
-                    mf(dic).SelectMany(c => bind(c)))
-            );
+            return fd.maybeGetPushMore.Fold(c => bind(c), mf => mf.Fold(addf =>
+                {
+                    var t = addf();
+                    return new FreeDict<A, B, D>(() => new Tuple<A, B, FreeDict<A, B, D>>(t.Item1, t.Item2, t.Item3.SelectMany(c => bind(c))));
+                }, getf =>
+                {
+                    var t = getf();
+                    return new FreeDict<A, B, D>(() => new Tuple<A, Func<B, FreeDict<A, B, D>>>(t.Item1, (b) => t.Item2(b).SelectMany(c => bind(c))));
+                }
+           ));
         }
-
-
     }
+     
 }
+
